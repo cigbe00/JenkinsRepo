ea7ad41b0b75564f2c15f72457863ce4c3302257
Yon <cigbe00@gmail.com>
2021-05-10 20:31:50 -0400

    Update MagicBuilder.java

--- com/chukwudi/example/MagicBuilder.java
+++ com/chukwudi/example/MagicBuilder.java
@@ -3,6 +3,7 @@ package com.chukwudi.example;
 public class MagicBuilder {
 
     public static int getLucky() {
+        System.out.println("this is the getLucky method");
         return 7;
     }
 
